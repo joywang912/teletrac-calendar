34a453aa67e36aa20282c710fc8784efb05dc315
Jue Wang <joywang912@hotmail.com>
2020-12-08 17:23:31 +1300

    Create Component: Calendar

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="app container">
-      <div className="row">
-        <div className="col-sm-4 left-conatiner" >
+      <div className="row no-gutters">
+        <div className="col-md-4 left-conatiner" >
           <Weather />
         </div>
-        <div className="col-sm-8" >
+        <div className="col-md-8" >
           <Calendar />
         </div>
       </div>
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import ReactCalendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 import './calendar.scss';
 
+
 const Calendar = () => {
+    const [date, setDate] = useState(new Date());
+
+    const onDateChange = (value: Date) => {
+        setDate(value);
+    };
+
     return <div className="calendar">
-        Calendar
+        <ReactCalendar
+            onClickDay={onDateChange}
+            value={date}
+        />
     </div>;
 };
 
